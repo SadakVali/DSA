595ab256d8ce5f98b7d7da09d62b41e57f4624a1
Sadak Vali <54495800+SadakVali@users.noreply.github.com>
2023-09-02 16:04:11 +0530

    Revert "CPP basics 2"
    This reverts commit 71f3312779321703c90c08c2d2f6688aa32f2012.

--- 01. C++ Language Basics/06. typeCasting.cpp
+++ 01. C++ Language Basics/06. typeCasting.cpp
@@ -11,14 +11,8 @@ int main()
   cout << b << endl;
 
   // explicit type conversion
-  float d = 5.7;
-  char cha = 'g';
-  float x = d + cha;
-  // char x = d + cha;
-  cout << "Value of x is : " << x << endl;
-
-  // double d = 5.7;
-  // int x = d + 2; // (int)d + 2;
-  // cout << "Value of x is : " << x << endl
-  //      << endl;
+  double d = 5.7;
+  int x = d + 2; // (int)d + 2;
+  cout << "Value of x is : " << x << endl
+       << endl;
 }
