830f2b2d34e6f1d566326431b98af92322a6a1ee
Sadak Vali <54495800+SadakVali@users.noreply.github.com>
2023-09-11 16:12:35 +0530

    week 1 assignment questions done

--- 02. C++ Language Basics/11. fullPyramid.cpp
+++ 02. C++ Language Basics/11. fullPyramid.cpp
@@ -4,25 +4,50 @@ using namespace std;
 int main()
 {
   int size;
-  cout << "Enter the size of the Full Pyramid : ";
+  cout << "Enter the size of the Half Pyramid : ";
   cin >> size;
   // observing each row
   for (int row = 0; row < size; row += 1)
   {
-    // observing num col
-    for (int col = 0; col <= (size - 1 + row); col += 1)
+    // observing each col
+    for (int space = 0; space < (size - 1 - row); space += 1)
     {
-      if (col < (size - 1 - row))
-      {
-        cout << " ";
-      }
-      else
-      {
-        cout << "* ";
-        col += 1;
-      }
+      cout << " ";
+    }
+    for (int col = 0; col < (row + 1); col += 1)
+    {
+      cout << "* ";
     }
     cout << endl;
   }
   cout << endl;
 }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//   int size;
+//   cout << "Enter the size of the Full Pyramid : ";
+//   cin >> size;
+//   // observing each row
+//   for (int row = 0; row < size; row += 1)
+//   {
+//     // observing num col
+//     for (int col = 0; col <= (size - 1 + row); col += 1)
+//     {
+//       if (col < (size - 1 - row))
+//       {
+//         cout << " ";
+//       }
+//       else
+//       {
+//         cout << "* ";
+//         col += 1;
+//       }
+//     }
+//     cout << endl;
+//   }
+//   cout << endl;
+// }
